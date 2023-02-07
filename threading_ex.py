e00488a06b43401c42b5132413b52442059d7508
Ankit Raj <102459970+ankit-raaz@users.noreply.github.com>
2023-02-07 15:32:12 +0530

    Add files via upload

---
+++ threading_ex.py
@@ -0,0 +1,35 @@
+#Multithreading
+import time 
+import threading
+
+def calc_square(numbers):
+	print("calculate of square numbers")
+	for n in numbers: 
+		time.sleep(1)
+		print('square:', n*n)
+
+def calc_cube(numbers):
+	print("calculate cube of numbers")
+	for n in numbers:
+		time.sleep(1)
+		print('cube:', n*n*n)
+		
+
+arr= [2,3,8,9]
+
+t=time.time()
+#Series 
+#calc_square(arr)
+#calc_cube(arr)
+
+#parallel
+t1=threading.Thread(target=calc_square, args=(arr,))
+t2=threading.Thread(target=calc_cube, args=(arr,))
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
+print("done in :" , time.time()-t)
+
